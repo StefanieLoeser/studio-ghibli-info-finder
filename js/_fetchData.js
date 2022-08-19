0916e2942b0da5ea1b690870e8ca600e2cef6686
StefanieLoeser <stefanie.loeser@gmx.net>
2022-08-19 18:06:21 +0200

    add image styling

--- js/_fetchData.js
+++ js/_fetchData.js
@@ -1,32 +1,33 @@
 const api = 'https://ghibliapi.herokuapp.com/films/';
 
 fetch(api)
-  .then(response => response.json())
-  .then(data => ghibliData(data));
+    .then(response => response.json())
+    .then(data => ghibliData(data));
 
 export default function ghibliData(data) {
-  const movies = data;
-  const gallery = document.querySelector('[data-js="movie"]');
-  const dropdown = document.querySelector('[data-js="dropdown"]');
-  console.log(movies);
+    const movies = data;
+    const gallery = document.querySelector('[data-js="movie"]');
+    const dropdown = document.querySelector('[data-js="dropdown"]');
+    console.log(movies);
 
-  movies.forEach(movie => {
-    const title = document.createElement('option');
-    title.textContent = movie.title;
-    title.setAttribute('value', movie.title);
-    dropdown.append(title);
+    movies.forEach(movie => {
+        const title = document.createElement('option');
+        title.textContent = movie.title;
+        title.setAttribute('value', movie.title);
+        dropdown.append(title);
 
-    const figure = document.createElement('figure');
-    gallery.append(figure);
+        const figure = document.createElement('figure');
+        figure.classList.add('movie__container--figure');
+        gallery.append(figure);
 
-    const movieImage = document.createElement('img');
-    movieImage.src = movie.image;
-    movieImage.setAttribute('alt', '');
-    movieImage.classList.add('movie__container--image');
-    figure.append(movieImage);
+        const movieImage = document.createElement('img');
+        movieImage.src = movie.image;
+        movieImage.setAttribute('alt', '');
+        movieImage.classList.add('movie__container--image');
+        figure.append(movieImage);
 
-    const movieTitle = document.createElement('figcaption');
-    movieTitle.textContent = movie.title;
-    figure.append(movieTitle);
-  });
+        const movieTitle = document.createElement('figcaption');
+        movieTitle.textContent = movie.title;
+        figure.append(movieTitle);
+    });
 }
