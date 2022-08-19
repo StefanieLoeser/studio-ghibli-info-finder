6e34a4d94bdc461742dbf439cd3d06960ad716c6
Andre Haupt <andrehaupt@me.com>
2022-08-19 16:35:28 +0200

    creat html from movie object info

--- js/_fetchData.js
+++ js/_fetchData.js
@@ -6,15 +6,27 @@ fetch(api)
 
 export default function ghibliData(data) {
   const movies = data;
+  const gallery = document.querySelector('[data-js="movie"]');
   const dropdown = document.querySelector('[data-js="dropdown"]');
-  
+  console.log(movies);
+
   movies.forEach(movie => {
     const title = document.createElement('option');
-    title.setAttribute('value', 'title.value');
-    title.textContent = title.value;
-    console.log(title);
-  });
-  
+    title.textContent = movie.title;
+    title.setAttribute('value', movie.title);
+    dropdown.append(title);
 
-  console.log(movies);
+    const figure = document.createElement('figure');
+    gallery.append(figure);
+
+    const movieImage = document.createElement('img');
+    movieImage.src = movie.image;
+    movieImage.setAttribute('alt', '');
+    movieImage.classList.add('movie__container--image');
+    figure.append(movieImage);
+
+    const movieTitle = document.createElement('figcaption');
+    movieTitle.textContent = movie.title;
+    figure.append(movieTitle);
+  });
 }
