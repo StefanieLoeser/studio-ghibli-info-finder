a97bede7323d3390de469fc1d21c37b1e013507c
Andre Haupt <andrehaupt@me.com>
2022-08-20 10:15:19 +0200

    add reset button

--- js/_movieDetails.js
+++ js/_movieDetails.js
@@ -2,7 +2,8 @@ import ghibliData from './_createElement.js';
 import { movieData } from './_fetchData.js';
 
 export default function showMovieDetails() {
-  const figures = document.querySelectorAll('[data-js="singleMovie"]');
+  const figures = document.querySelectorAll('[data-js=singleMovie]');
+  const resetButton = document.querySelector('[data-js=resetButton]');
   figures.forEach(figure => {
     figure.addEventListener('click', () => {
       const figureCaption = figure.querySelector('[data-js="caption"]');
@@ -10,6 +11,7 @@ export default function showMovieDetails() {
         movieData.find(o => o.title === figureCaption.textContent),
       ];
       ghibliData(movieTitle);
+      resetButton.classList.remove('hidden');
     });
   });
 }
--- js/_selectContent.js
+++ js/_selectContent.js
@@ -1,8 +1,10 @@
 import { movieData } from './_fetchData.js';
 import ghibliData from './_createElement.js';
+import showMovieDetails from './_movieDetails.js';
 
 export default function selector() {
   const dropdown = document.querySelector('[data-js=dropdown]');
+  const reset = document.querySelector('[data-js=resetButton]');
   dropdown.addEventListener('change', event => {
     const filteredMovie = movieData.filter(
       movie => movie.title === event.target.value
@@ -10,8 +12,10 @@ export default function selector() {
 
     if (filteredMovie.length > 0) {
       ghibliData(filteredMovie);
+      reset.classList.remove('hidden');
     } else {
       ghibliData(movieData);
+      showMovieDetails();
     }
 
     console.log(filteredMovie);
