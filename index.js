21a2f0e63b4d4a75a47c51095a1afcc302f90191
Andre Haupt <andrehaupt@me.com>
2022-08-22 09:03:12 +0200

    add scroll buttons

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import ghibliData from './js/_createElement.js';
 import selector from './js/_selectContent.js';
 import showMovieDetails from './js/_movieDetails.js';
 import resetSelect from './js/_resetSelection.js';
+import scroll from './js/_scroll.js'
 
 ghibliData(movieData);
 selector();
 showMovieDetails();
 resetSelect();
+scroll();
